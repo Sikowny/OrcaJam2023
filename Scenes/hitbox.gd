16b3a22aba9c3b9941b108343591f4bedfbf2677
Sikowny <bsickowny@hotmail,com>
2023-10-20 20:41:50 -0700

    update input. hitboxes

---
+++ Scenes/hitbox.gd
@@ -0,0 +1,19 @@
+extends Area3D
+
+@export var SPEED: float = 0.0
+@export var direction: Vector3 = Vector3.ZERO
+
+var lifeTime = 120;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	#reparent($"../..")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position += direction * SPEED
+	if(lifeTime <= 0): queue_free()
+	if(lifeTime): lifeTime -= 1
+	
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -6,16 +6,24 @@ const JUMP_VELOCITY = 4.5
 const FRICTION = 1.0
 const ACCELERATION = 1.0
 
+@onready var spring_arm: SpringArm3D = $SpringArm3D
+@onready var facing_ray: RayCast3D = $Facing
+var hitbox = preload("res://Scenes/hitbox.tscn")
+
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-@onready var spring_arm: SpringArm3D = $SpringArm3D
 
 var input_move: Vector2 = Vector2.ZERO;
+
+var facing: Vector3 = Vector3.ZERO;
 
 
 func _ready() -> void:
 	spring_arm.spring_length = 15;
 	spring_arm.rotation.x = -45;
+	facing_ray.target_position = Vector3(0,0,4);
+	
 
 
 func _physics_process(delta: float) -> void:
@@ -36,9 +44,11 @@ func get_player_input():
 func handle_jump():
 	if Input.is_action_just_pressed("key_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		create_hitbox()
 
 func handle_movement():
 	var direction := (transform.basis * Vector3(input_move.x, 0, input_move.y)).normalized()
+	update_facing(direction)
 	if direction:
 		velocity.x = move_toward(velocity.x, direction.x * SPEED, ACCELERATION)
 		velocity.z = move_toward(velocity.z, direction.z * SPEED, ACCELERATION)
@@ -48,3 +58,13 @@ func handle_movement():
 		
 func handle_gravity(delta):
 	if not is_on_floor(): velocity.y -= gravity * delta
+	
+func update_facing(direction):
+	if direction == Vector3.ZERO: return;
+	facing = direction.normalized()
+	facing_ray.target_position = facing*4 #for debuging
+	
+func create_hitbox():
+	var new_hitbox = hitbox.instantiate()
+	new_hitbox.position = position
+	add_sibling(new_hitbox) # add this node to parent of player
