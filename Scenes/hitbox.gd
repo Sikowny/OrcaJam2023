2bd260115a1169363c51afced0759c53018232d1
Sikowny <bsickowny@hotmail,com>
2023-10-20 21:43:20 -0700

    end of friday tune ups

--- Scenes/hitbox.gd
+++ Scenes/hitbox.gd
@@ -3,13 +3,14 @@ extends Area3D
 @export var SPEED: float = 0.0
 @export var direction: Vector3 = Vector3.ZERO
 
+#Dust, Wipe, Vacumm
+var attackType = "Dust"
 var lifeTime = 120;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
 	#reparent($"../..")
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- Scenes/player.gd
+++ Scenes/player.gd
@@ -6,11 +6,15 @@ const JUMP_VELOCITY = 4.5
 const FRICTION = 1.0
 const ACCELERATION = 1.0
 
+#grab refences to connected nodes on this Scene/Object
 @onready var spring_arm: SpringArm3D = $SpringArm3D
 @onready var facing_ray: RayCast3D = $Facing
+
+#preload another object to instanciate it later
 var hitbox = preload("res://Scenes/hitbox.tscn")
 
 
+#set up vars
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -30,6 +34,7 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	handle_gravity(delta)
 	handle_jump()
+	handle_attack()
 	get_player_input()
 	
 	# Get the input direction and handle the movement/deceleration.
@@ -44,7 +49,10 @@ func get_player_input():
 func handle_jump():
 	if Input.is_action_just_pressed("key_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		create_hitbox()
+		
+func handle_attack():
+	if Input.is_action_just_pressed("key_action2"):
+		create_hitbox(facing + Vector3(0,1,0))
 
 func handle_movement():
 	var direction := (transform.basis * Vector3(input_move.x, 0, input_move.y)).normalized()
@@ -64,7 +72,7 @@ func update_facing(direction):
 	facing = direction.normalized()
 	facing_ray.target_position = facing*4 #for debuging
 	
-func create_hitbox():
+func create_hitbox(positionOffset:Vector3 = Vector3(0,1,0)):
 	var new_hitbox = hitbox.instantiate()
-	new_hitbox.position = position
-	add_sibling(new_hitbox) # add this node to parent of player
+	new_hitbox.position = position + positionOffset
+	add_sibling(new_hitbox) # add this node to parent of the parent of player.
